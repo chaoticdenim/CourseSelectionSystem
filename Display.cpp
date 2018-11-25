6862a34668e2f38d6f1ca95761cca20c8b2d809d
Guillaume Brizolier <guillaume.brizolier@etu.emse.fr>
2018-11-25 23:36:32 +0100

    add planning displaying

--- Display.cpp
+++ Display.cpp
@@ -1,8 +1,4 @@
 #include "Display.h"
-#include <iostream>
-#include <sstream>
-#include <fstream>
-#include <map>
 using namespace std;
 
 bool Display::loginScreen()
@@ -17,6 +13,7 @@ bool Display::loginScreen()
 	{
 		cout << "Login: " << endl;
 		cin >> login;
+		this->login = login;
 		cout << "Password: " << endl;
 		cin >> password;
 
@@ -32,9 +29,9 @@ bool Display::loginScreen()
 			users.insert(pair<string, string>(flogin, fpassword));
 		}
 
-		if (users.find(login) != users.end())
+		if (users.find(this->login) != users.end())
 		{
-			if (users.find(login)->second == password)
+			if (users.find(this->login)->second == password)
 			{
 				result = true;
 				return result;
@@ -42,12 +39,16 @@ bool Display::loginScreen()
 			else
 			{
 				cout << "wrong password" << endl;
+				return result;
 			}
 		}
 		else
 		{
 			cout << "user not found" << endl;
+			return result;
 		}
+
+		return result;
 	}	
 }
 
@@ -63,6 +64,7 @@ void Display::menu()
 	{
 	case 1:
 		cout << "You choose Planning" << endl;
+		planningScreen();
 		break;
 	case 2:
 		cout << "You chose Course choice" << endl;
@@ -70,6 +72,87 @@ void Display::menu()
 	case 3:
 		cout << "You logged out" << endl;
 		break;
+	}
+}
+
+void Display::planningScreen()
+{
+	fstream infile("planning.txt"); //line impaire = login. Ligne paire = vecteur. 1er élément : nbre de cours suivis. le reste : indique si le cours i est suivi ou non
+	map<string, vector<int>> loginToPlanning;
+	vector<int> coursesFollowed;
+	vector<string> courseNames = { "Math", "Physics", "VHDL", "Another Useless One", "Haskell Programming", "SAM" };
+	string line;
+	string login;
+	bool isLineEven = false;
+	int isFollowed;
+	string s_isFollowed;
+
+	if (infile.fail())
+	{
+		cout << "error opening file" << endl;
+	}
+	else
+	{
+		while (getline(infile, line)) //Lecture des données sauvegardées dans planning.txt et restitution dans la map.
+		{
+			if (isLineEven)
+			{
+				stringstream linestream(line);
+				while (getline(linestream, s_isFollowed, ' '))
+				{
+					coursesFollowed.push_back(stoi(s_isFollowed));
+				}
+
+				loginToPlanning.insert(make_pair(login, coursesFollowed));
+				coursesFollowed.clear();
+				isLineEven = false;
+			}
+			else
+			{
+				login = line;
+				isLineEven = true;
+			}
+		}
+
+		infile.close();
+
+		cout << "current login : " << this->login << endl;
+
+		map<string, vector<int>>::iterator found = loginToPlanning.find(this->login);
+
+		if (found == loginToPlanning.end())
+		{
+			cout << "No courses taken. Planning created. You can now enroll in courses" << endl;
+ 			fstream outfile("planning.txt", fstream::app | fstream::out);
+			if (outfile.fail())
+			{
+				cout << "error creating file" << endl;
+			}
+			else
+			{
+				outfile << endl << this->login << endl;
+				for (int i = 0; i < 7; i++)
+				{
+					outfile << "0 ";
+				}
+			}
+
+			outfile.close();
+		}
+		else //Display enrolled courses
+		{
+			cout << "You are enrolled in " << *found->second.begin() << " courses" << endl;
+			int i = 0;
+			for (auto it = found->second.begin() + 1; it != found->second.end(); it++)
+			{
+				if (*it == 1)
+				{
+					cout << courseNames[i] << ";";
+				}
+				i++;
+			}
+			cout << endl;
+		}
 	}
 }
 
--- Display.h
+++ Display.h
@@ -1,9 +1,21 @@
 #pragma once
+#include <sstream>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <map>
+#include <vector>
+using namespace std;
+
 class Display
 {
 public:
 	bool loginScreen();
 	void menu();
+	void planningScreen();
 	void clearScreen();
+private:
+	string login;
+	vector<int> courseVector = { 0, 0, 0, 0, 0, 0, 0 };
 };
 
